3340a56d5cb674c24865ddec4348eca485e9aa7c
jessicamcg <jessicamcguico@gmail.com>
2021-11-01 15:49:36 -0500

    blog routes post method good

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,5 +1,30 @@
 const router = require('express').Router();
-const { Blog } = require('../../models');
+const { User, Blog } = require('../../models');
 
+router.post('/:id', async (req, res) => {
+    try {
+        const userData = await User.findOne({
+            where: {
+                id: req.params.id,
+            },
+        })
+
+        if (!userData) {
+            res.status(404).json({message: 'No user with that id'})
+        }
+
+
+        const blogData = await Blog.create({
+            title: req.body.title,
+            content: req.body.content,
+            date_posted: Date.now(),
+            user_id: userData.dataValues.id,
+        });
+
+        res.status(200).json(blogData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
--- public/js/newPost.js
+++ public/js/newPost.js
@@ -1,8 +1,34 @@
+const newpostBtnHandler = async (event) => {
+
+};
+
+
 const newpostFormHandler = async (event) => {
     event.preventDefault();
-    document.location.replace('/newblog')
+
+    const title = document.querySelector('#new-blog-title').value.trim();
+    const content = document.querySelector('#new-blog-content').value.trim();
+
+    const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ title,content }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+    
+
+    if (response.ok) {
+        document.location.replace('/newblog');
+    } else {
+        alert('Error: Could not create new blog post');
+    }
 };
+
+
 
 document
     .querySelector('#new-blog-btn')
+    .addEventListener('click', newpostBtnHandler);
+
+    document
+    .querySelector('#create-new-blog-btn')
     .addEventListener('click', newpostFormHandler)
