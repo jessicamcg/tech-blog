219e0c3f73018bbe485307855edeebb4f19a9e35
jessicamcg <jessicamcguico@gmail.com>
2021-10-31 22:54:21 -0500

    renders blog on homepage, still needs css and js

--- seeds/blogData.js
+++ seeds/blogData.js
@@ -1,7 +1,11 @@
 const { Blog } = require('../models');
 
 const blogData = [
-
+    {
+        title: 'Object-Relational Mapping',
+        content: 'I have really loved learning about ORMs. It has really simplified the way I create queries in SQL!',
+        date_posted: '10/31/2021',
+    }
 ];
 
 const seedBlog = () =>Blog.bulkCreate(blogData);
