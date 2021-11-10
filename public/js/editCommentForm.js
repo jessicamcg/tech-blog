6cfee016eb2df45e642e8fd7986d315d8ac2fa65
jessicamcg <jessicamcguico@gmail.com>
2021-11-09 18:37:49 -0600

    edit form put method functions

--- public/js/editCommentForm.js
+++ public/js/editCommentForm.js
@@ -18,26 +18,25 @@ const windowID = windowArr[windowArr.length - 1];
 // console.log("windowid:", windowID);
 commentView(windowID);
 
-// const editCommentFormHandler = async (event) => {
-//     event.preventDefault();
-//     const title = document.querySelector('#new-comment-title').value.trim();
-//     const content = document.querySelector('#new-comment-content').value.trim();
-//     if (title && content) {
-//         const response = await fetch('/api/comments', {
-//             method: 'PUT',
-//             body: JSON.stringify({ title,content }),
-//             headers: { 'Content-Type': 'application/json' },
-//         });
+const editCommentFormHandler = async (event) => {
+    event.preventDefault();
+    const title = document.querySelector('#edit-comment-title').value.trim();
+    const content = document.querySelector('#edit-comment-content').value.trim();
+    if (title && content) {
+        const response = await fetch(`/api/comments/${windowID}`, {
+            method: 'PUT',
+            body: JSON.stringify({ title,content }),
+            headers: { 'Content-Type': 'application/json' },
+        });
         
-//         if (response.ok) {
-//             // document.location.replace('/dashboard');
-//             history.back(); 
-//         } else {
-//             alert('Error: Could not create new comment');
-//         }
-//     }
-// };
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Error: Could not create new comment');
+        }
+    }
+};
 
-// document
-//     .querySelector('.edit-comment-btn-form')
-//     .addEventListener('submit', editCommentFormHandler);
+document
+    .querySelector('.edit-comment-btn-form')
+    .addEventListener('submit', editCommentFormHandler);
